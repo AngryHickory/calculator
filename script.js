ec003530a429d5dc5aa576f5139ae5d9efe76852
AngryHickory <138169281+AngryHickory@users.noreply.github.com>
2025-05-31 06:01:02 -0700

    Comeplete operate function
    This is a function that can use the math equation functions.
    It will be used to input calculations into the calculator
    once the buttons are done.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const input1 = "";
+const input2 = "";
+const operator = "";
+
+
 function add(a, b) {
     return a + b;
 }
@@ -12,7 +17,21 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b === 0) {
-        return "error";
+        return "error - can't divide by 0";
     }
     return a / b;
 }
+
+function operate(a, b, operator) {
+    if (operator == "+") {
+        return add(a, b);
+    } else if (operator == "-") {
+        return subtract(a, b);
+    } else if (operator == "*") {
+        return multiply(a, b);
+    } else {
+        return divide(a, b);
+    }
+
+}
+console.log(operate(1, 0, "*"));
